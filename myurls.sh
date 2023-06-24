c654ec2224996759defdf709b8d21b31d760720c
kankankankankankan <vip029@me.com>
2023-06-24 18:51:29 +0800

    Update myurls.sh

--- myurls.sh
+++ myurls.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sed -i "s#http://example.com#https://$DOMAIN#g" ./public/index.html
+sed -i '22s/.*/<img width="250" src="https:\/\/cdn.jsdelivr.net\/gh\/kankankankankankan\/MyUrls@master\/public\/logo.png"\/>/' ./public/index.html
 ./linux-amd64-myurls.service -domain $DOMAIN -conn $RDSHOST:$RDSPORT -passwd $RDSPASSWORD -ttl $TTL &
 nginx -g 'daemon off;'
