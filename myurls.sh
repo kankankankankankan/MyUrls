1e9ac85b3807018a2c5beff7e8a12134a4e2a63e
kankankankankankan <vip029@me.com>
2023-06-24 19:05:01 +0800

    Update myurls.sh

--- myurls.sh
+++ myurls.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sed -i "s#http://example.com#https://$DOMAIN#g" ./public/index.html
+sed -i '7s/MyUrls/URLS/' ./public/index.html
 sed -i '22s/.*/<img width="250" src="https:\/\/cdn.jsdelivr.net\/gh\/kankankankankankan\/MyUrls@master\/public\/logo.png"\/>/' ./public/index.html
 sed -i '23s/@keyup.enter.native="enterToDoShort"/@keyup.enter.native="enterToDoShort" placeholder="请输入转换链接"/' ./public/index.html
 ./linux-amd64-myurls.service -domain $DOMAIN -conn $RDSHOST:$RDSPORT -passwd $RDSPASSWORD -ttl $TTL &
